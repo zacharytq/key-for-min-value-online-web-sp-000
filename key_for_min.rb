3ca9562d47db102bf75d01a1a2f6bd5d66c4fd1b
chic-page-6502 <zacharytq@gmail.com>
2019-07-16 02:27:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,11 @@
 
 def key_for_min_value(name_hash)
   return_value = ""
-  number = 0
+  number = 1000
   name_hash.each do |name, value|
     if value < number
       return_value = name
+      number = value
     end
   end
   return_value
